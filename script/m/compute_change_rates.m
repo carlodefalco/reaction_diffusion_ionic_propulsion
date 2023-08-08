8c05ca7d49190730a1950223af526d325705e3af
unknown <giuseppe.calio@mail.polimi.it>
2023-08-08 18:38:29 +0200

    compute change rates in implict form suitable for ode15i

--- script/m/compute_change_rates.m
+++ script/m/compute_change_rates.m
@@ -22,15 +22,15 @@
 ##
 ## with forward reaction coefficient cf_i and backward reaction coefficient cb_i
 ##
-## the rate Rf_i of the i-th forward reaction is 
+## the rate Rf_i of the i-th forward reaction is
 ##
 ## Rf_i = cf_i \prod_k s_k^{\left( r_{i,k} \right)}
 ##
-## the rate Rb_i of the i-th forward reaction is 
+## the rate Rb_i of the i-th forward reaction is
 ##
 ## Rb_i = cb_i \prod_k s_k^{\left( p_{i,k} \right)}
 ##
-## the net rate of the i-th reaction is 
+## the net rate of the i-th reaction is
 ##
 ## R_i = Rf_i - Rb_i
 ##
@@ -38,14 +38,21 @@
 ##
 ## \dot{s}_k = \sum_{i=1}^M \left( - Rf_i r_{i,k} + Rf_i p_{i,k}
 ##             - Rb_i p_{i,k} - Rb_i r_{i,k} \right) =
-##             - \sum_{i=1}^M \left( R_i (r_{i,k} - p_{i,k}) \right) 
+##             - \sum_{i=1}^M \left( R_i (r_{i,k} - p_{i,k}) \right)
+##the system is rw-written in an implicit form of type F(t, y, ydot)=0
+##So:
+## \dot{s}_k - \sum_{i=1}^M \left( - Rf_i r_{i,k} + Rf_i p_{i,k}
+##             - Rb_i p_{i,k} - Rb_i r_{i,k} \right) =
+##             - \sum_{i=1}^M \left( R_i (r_{i,k} - p_{i,k}) \right)=0
+##the last equations is replaced with a costraint equation (conservation of mass)
 
 
 
-function dstate = compute_change_rates (state, reactions, index)
 
-  dstate = zeros (size (state));
-  for the_reaction = reactions(:)'
+function implicit_eqs = compute_change_rates (state, dstate, reactions, index)
+  Ri_vec=[];
+  sum_Ri = zeros (size (state));
+ for the_reaction = reactions(:)'
 
     Rfi = the_reaction.rate_coeffs(1);
     for [value, key] = the_reaction.reactants
@@ -60,12 +67,16 @@ function dstate = compute_change_rates (state, reactions, index)
     Ri = Rfi - Rbi;
 
     for [value, key] = the_reaction.reactants
-      dstate (index.(key)) -= Ri * value;
+      sum_Ri (index.(key)) -= Ri * value;
+      %Ri_vec= [Ri_vec; -Ri*value];
     endfor
     for [value, key] = the_reaction.products
-      dstate (index.(key)) += Ri * value;
+      sum_Ri (index.(key)) += Ri * value;
+      %Ri_vec= [Ri_vec; Ri*value];
     endfor
-    
-  endfor
+
+ endfor
+  implicit_eqs = dstate-sum_Ri;
+
 
 endfunction
