d9f202d843e569d7e60fbe0f774dc6998314c9fc
unknown <giuseppe.calio@mail.polimi.it>
2023-11-24 19:17:02 +0100

    remove some files

--- script/m/IPROP_ARGON_SPLITTING_MIXED_BCs/Integrate/templatejac.m
+++
@@ -1,6 +0,0 @@
-function [dfdy, dfdydot] = templatejac (dy, dydot, t, y, ydot)
-    dfdy = dy (t, y, ydot) ;
-    if (nargout>1)
-      dfdydot = dydot (t, y, ydot) ;
-    endif
-endfunction
--- script/m/IPROP_ARGON_SPLITTING_MIXED_BCs/Integrate/trap_step.m
+++
@@ -1,7 +0,0 @@
-function u1 = trap_step (u0, t, dt, rate, jacrate, mass)
-  b  = mass * u0 + dt * (.001 * rate (u0, t) + .999 * (rate (u0, t) - jacrate (u0, t) * u0)) ;
-  A  = mass - .999 * dt * jacrate (u0, t);
-
-  u1 = full(A) \ b;
-endfunction
-
